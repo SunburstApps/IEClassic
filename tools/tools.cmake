dc16f6e87f50b97b57770117239169c25cc009f0
William Kent <wjk011@gmail.com>
2017-10-11 22:34:48 -0400

    Add target_add_manifest() CMake function
    Also added the manifest XML files used as input to this function.

--- tools/tools.cmake
+++ tools/tools.cmake
@@ -1,3 +1,5 @@
+include(CMakeParseArguments)
+
 function(spec2def _dllname _spec_file)
 	get_filename_component(_file ${_dllname} NAME_WE)
 
@@ -23,4 +25,29 @@ function(spec2def _dllname _spec_file)
 		OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_file}.def ${CMAKE_CURRENT_BINARY_DIR}/${_file}_stubs.c
 		COMMAND $<TARGET_FILE:spec2def> ${_ms_flag} -a=${SPEC2DEF_ARCH} -n=${_dllname} -d=${CMAKE_CURRENT_BINARY_DIR}/${_file}.def -s=${CMAKE_CURRENT_BINARY_DIR}/${_file}_stubs.c ${_spec_file}
 		DEPENDS ${_spec_file} spec2def)
+endfunction()
+
+function(target_add_manifest target)
+	cmake_parse_arguments(_mt "ENABLE_VISUAL_THEMES;ENABLE_HIDPI_SUPPORT;ENABLE_VERSIONING" "" "ADDITIONAL_FRAGMENTS" ${ARGN})
+	set(tools_dir ${REACTOS_INTERNET_EXPLORER_SOURCE_DIR}/tools)
+
+	set(_manifest_files ${_mt_ADDITIONAL_FRAGMENTS})
+	if(_mt_ENABLE_VISUAL_THEMES)
+		list(APPEND _manifest_files "${tools_dir}/manifests/visual_themes.manifest")
+	endif()
+	if(_mt_ENABLE_HIDPI_SUPPORT)
+		list(APPEND _manifest_files "${tools_dir}/manifests/hidpi_support.manifest")
+	endif()
+	if(_mt_ENABLE_VERSIONING)
+		list(APPEND _manifest_files "${tools_dir}/manifests/versioning.manifest")
+	endif()
+
+	list(LENGTH _manifest_files manifest_file_count)
+	if(manifest_file_count EQUAL 0)
+		message(AUTHOR_WARNING "No manifest files specified, target_add_manifest() won't do anything")
+	else()
+		add_custom_command(TARGET ${target} POST_BUILD
+			COMMAND mt.exe -nologo -manifest ${_manifest_files} -outputresource:$<TARGET_FILE:${target}> -canonicalize
+			DEPENDS ${_manifest_files} ${target})
+	endif()
 endfunction()
